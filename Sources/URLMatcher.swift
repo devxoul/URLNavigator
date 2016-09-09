5cb1b0d8b50cb5244e9b90d9b8026f31db3b70c7
Josh Sklar <jrmsklar@gmail.com>
2016-09-09 14:07:49 -0400

    use correct local constant name

--- Sources/URLMatcher.swift
+++ Sources/URLMatcher.swift
@@ -141,7 +141,7 @@ public class URLMatcher {
         let URLString = URL.URLStringValue
         if let scheme = scheme where !URLString.containsString("://") {
             #if DEBUG
-                if !URLPatternString.hasPrefix("/") {
+                if !URLString.hasPrefix("/") {
                     NSLog("[Warning] URL pattern doesn't have leading slash(/): '\(URL)'")
                 }
             #endif
