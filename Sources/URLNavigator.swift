06176c7ce1ffa04663d0b9b082901011ef31c7df
Suyeol Jeon <devxoul@gmail.com>
2017-07-03 16:39:47 +0900

    Prevent from pushing UINavigationController

--- Sources/URLNavigator.swift
+++ Sources/URLNavigator.swift
@@ -194,6 +194,7 @@ open class URLNavigator {
     guard let navigationController = from ?? UIViewController.topMost?.navigationController else {
       return nil
     }
+    guard (viewController is UINavigationController) == false else { return nil }
     navigationController.pushViewController(viewController, animated: animated)
     return viewController
   }
