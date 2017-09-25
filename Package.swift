1d0e125b83a93011e9f3f939bd9280b56915ab0b
Suyeol Jeon <devxoul@gmail.com>
2017-09-25 18:57:34 +0900

    Update Package.swift to V4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-  name: "URLNavigator"
+  name: "URLNavigator",
+  products: [
+    .library(name: "URLNavigator", targets: ["URLNavigator"]),
+  ],
+  targets: [
+    .target(name: "URLNavigator"),
+    .testTarget(name: "URLNavigatorTests", dependencies: ["URLNavigator"]),
+  ]
 )
