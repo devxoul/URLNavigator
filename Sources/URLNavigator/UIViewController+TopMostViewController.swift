1ac8b230c7dd91ed0555f3fbb0a67b34249a5d99
RyogaK <RyogaK@users.noreply.github.com>
2018-03-12 12:39:41 +0900

    Add parenthesis.

--- Sources/URLNavigator/UIViewController+TopMostViewController.swift
+++ Sources/URLNavigator/UIViewController+TopMostViewController.swift
@@ -9,7 +9,7 @@ extension UIViewController {
 
   /// Returns the current application's top most view controller.
   open class var topMost: UIViewController? {
-    guard let currentKeyWindows = self.sharedApplication?.windows.filter { $0.isKeyWindow } else { return nil }
+    guard let currentKeyWindows = self.sharedApplication?.windows.filter ({ $0.isKeyWindow }) else { return nil }
     var rootViewController: UIViewController?
     for window in currentKeyWindows {
       if let windowRootViewController = window.rootViewController {
