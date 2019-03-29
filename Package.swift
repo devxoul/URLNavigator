f18c1e5ccd0a35cb5685299d6586e8e0f11f60e6
Suyeol Jeon <devxoul@gmail.com>
2019-03-29 18:37:36 +0900

    Update package dependencies to temporarily support Swift 5

--- Package.swift
+++ Package.swift
@@ -4,14 +4,17 @@ import PackageDescription
 
 let package = Package(
   name: "URLNavigator",
+  platforms: [
+    .iOS(.v8), .tvOS(.v9),
+  ],
   products: [
     .library(name: "URLMatcher", targets: ["URLMatcher"]),
     .library(name: "URLNavigator", targets: ["URLNavigator"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.2.0")),
-    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.2")),
-    .package(url: "https://github.com/devxoul/Stubber.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/devxoul/Quick.git", .branch("swift-5")),
+    .package(url: "https://github.com/devxoul/Nimble.git", .branch("swift-5")),
+    .package(url: "https://github.com/devxoul/Stubber.git", .upToNextMajor(from: "1.4.0")),
   ],
   targets: [
     .target(name: "URLMatcher"),
