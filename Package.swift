116c84a4f38beb5cf3d00dd842727614cecd2689
OhKanghoon <ggaa96@naver.com>
2022-06-09 01:17:13 +0900

    chore: update quick nimble versions

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
   name: "URLNavigator",
   platforms: [
-    .iOS(.v8), .tvOS(.v9),
+    .iOS(.v9), .tvOS(.v9),
   ],
   products: [
     .library(name: "URLMatcher", targets: ["URLMatcher"]),
     .library(name: "URLNavigator", targets: ["URLNavigator"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.1.0")),
-    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.2")),
+    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "10.0.0")),
     .package(url: "https://github.com/devxoul/Stubber.git", .upToNextMajor(from: "1.4.0")),
   ],
   targets: [
--- Tests/URLNavigatorTests/TestConfiguration.swift
+++ Tests/URLNavigatorTests/TestConfiguration.swift
@@ -2,7 +2,7 @@ import Quick
 import Stubber
 
 final class TestConfiguration: QuickConfiguration {
-  override class func configure(_ configuration: Configuration) {
+  override class func configure(_ configuration: QCKConfiguration) {
     configuration.beforeEach {
       Stubber.clear()
     }
